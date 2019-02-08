5f5b9ef2e00e320089a5def384dea83747525f91
Nidhin Benjamine <nbenjamine@vcg-nbe-mac.local>
2019-02-08 12:42:13 -0500

    initial spring boot admin service

---
+++ springboot-admin-dashboad/src/main/java/com/nbenja/store/springbootadmindashboad/SpringbootAdminDashboadApplication.java
@@ -0,0 +1,16 @@
+package com.nbenja.store.springbootadmindashboad;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class SpringbootAdminDashboadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAdminDashboadApplication.class, args);
+	}
+
+}
+
